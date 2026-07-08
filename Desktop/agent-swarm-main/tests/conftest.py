125574644a9af355a1da7429d695aa4c1de81ca6
Farhan Almas Dhrubo <farhaiee123@gmail.com>
2026-07-08 15:00:34 +0600

    Delete Desktop directory

--- Desktop/agent-swarm-main/tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from swarm.providers.factory import ProviderFactory
-
-
-@pytest.fixture(autouse=True)
-async def close_provider_clients_after_test():
-    yield
-    await ProviderFactory.close_cached()
